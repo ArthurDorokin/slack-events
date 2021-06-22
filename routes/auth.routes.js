d019190669aa1ed6ed2ec85d57a764b8d0220b9d
rbairamov@rightandabove.com <glQwerty!1>
2021-06-22 16:31:02 +0300

    slack

---
+++ routes/auth.routes.js
@@ -0,0 +1,45 @@
+const {Router} = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+const router = Router()
+// /api/auth/register
+router.post('/register' ,async (req, res) => {
+    try {
+        const {email, password} = req.body
+
+        const candidate = await User.findOne({email})
+
+        if (candidate) {
+           return res.status(400).json({message: 'Такой поьзыватель есть'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({email, password: hashedPassword})
+
+        await user.save();
+
+        res.status(201).json({message: 'пользыватель создан'})
+
+    } catch (e) {
+        res.status(500).json({message: 'Что-то пошло не так'})
+    }
+})
+
+// /api/auth/login
+router.post('/login' ,async (req, res) => {
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+
+        if (!user) {
+            return res.status(400).json({message: 'Такой поьзыватель NO есть'})
+        }
+
+
+
+    } catch (e) {
+        res.status(500).json({message: 'Что-то пошло не так again'})
+    }
+})
+
+module.exports = router
