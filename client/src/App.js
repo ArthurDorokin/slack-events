84b3e88c1d0d41faf7335fa9141b7d9e3556c97b
rbairamov@rightandabove.com <glQwerty!1>
2021-06-22 18:31:53 +0300

    slack

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
-import addToSlack from "./componens/pages/add_to_slack";
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" component={addToSlack}/>
-                </Switch>
-            </BrowserRouter>
+  return (
+    <div className="App">
+        <div className="slack">
+        <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands,chat:write&client_id=2181765481255.2193799443541"><img
+             height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
+            srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"/></a>
         </div>
-    );
+    </div>
+  );
 }
 
 export default App;
